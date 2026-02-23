fe556eec20dea8182b2544f0bdfbbe9e2af3e26e
ajmills1 <am3467@cam.ac.uk>
2026-02-23 15:05:04 +0000

    imporved last

--- task3.py
+++ task3.py
@@ -1,21 +1,22 @@
-import random
-import numpy as np
+def find_pi(N):
+    import random
+    a = 1
+    b = 1
+    N = N
+    counter = 0
 
-def f(x,y):
-    return x**2 + y**2 
+    def f(x,y):
+        return x**2 + y**2 
 
-a = 1
-b = 1
-N = 10**6
-counter = 0
+    for n in range(N):
+        x = random.uniform(-a,a)
+        y = random.uniform(-b,b)
+        v = f(x,y)
 
-for n in range(N):
-    x = random.uniform(-a,a)
-    y = random.uniform(-b,b)
-    v = f(x,y)
+        if v <= 1:
+            counter += 1
 
-    if v <= 1:
-        counter += 1
+    pi = 4*(counter/N)
+    return print(pi)
 
-pi = 4*(counter/N)
-print(pi)
+find_pi(10**5)
